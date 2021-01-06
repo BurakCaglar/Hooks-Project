b72fa2870e99720076ed673a5d7fd6ee6d2e6bd5
burakcaglar <burakcaglar@yandex.com>
2021-01-06 20:04:11 +0300

    nav menu data added

---
+++ src/data/MenuData.js
@@ -0,0 +1,11 @@
+export const menuData = [
+  { title: "Courses", icon: "/images/icons/courses.svg", link: "/courses" },
+  {
+    title: "Tutorials",
+    icon: "/images/icons/tutorials.svg",
+    link: "/tutorials",
+  },
+  { title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
+  { icon: "/images/icons/search.svg", link: "/search" },
+  { icon: "/images/icons/account.svg", link: "/account" },
+]
